3c107eb1b5bce88cefd49fd9dea3a3620d5437fd
panucitelcz <petr.b257@seznam.cz>
2025-09-22 13:44:49 +0200

    Prvni metoda

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 class car:
-    znacka = "" 
 
-    def __init__(self, znacka):
+    # Je možnost, kontrolovat si vstupní datové typy, které příjdou při instanci do objektu.
+    # Zamezíme tím případným potížím.
+    def __init__(self, znacka: str, rok: int, model: str, barva: str,typ_prevodovky: str, cena: float):
         self.znacka = znacka
+        self.rok = rok
+        self.model = model
+        self.barva = barva
+        self.typ_prevodovky = typ_prevodovky
+        self.cena = cena
 
-audi = car("Audi") #Instance
-skoda = car("Škoda")
+    def vypis(self):
+        return f"Název auta: {self.znacka}, Rok výroby: {self.rok} "
 
-print(audi.znacka)
-print(skoda.znacka)
+audi = car("Audi", 1999,"A4", "stříbrná", "Manuální", 45000) #Instance
+skoda = car("Škoda", 2014, "Superb II", "Bílá", "Manuální", 310000)
+
+print(audi.cena)
+print(skoda.model)
+print(audi.vypis())
