7d6906e2a084f6a2f8c2b95c49b593b17fe51996
panucitelcz <petr.b257@seznam.cz>
2025-09-25 09:32:37 +0200

    Treti hodina

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 class car:
+    """
+    Zadejte základní parametry auta
+    \nCenu auta udáváme v DPH
+    """
 
+    
     # Je možnost, kontrolovat si vstupní datové typy, které příjdou při instanci do objektu.
     # Zamezíme tím případným potížím.
     def __init__(self, znacka: str, rok: int, model: str, barva: str,typ_prevodovky: str, cena: float):
@@ -10,12 +15,23 @@ class car:
         self.typ_prevodovky = typ_prevodovky
         self.cena = cena
 
-    def vypis(self):
-        return f"Název auta: {self.znacka}, Rok výroby: {self.rok} "
+    def cena_bez_dph(self):
+        """ Vypočítá cenu auta bez DPH"""
+        return f" {self.cena * 0.79} kč"
 
-audi = car("Audi", 1999,"A4", "stříbrná", "Manuální", 45000) #Instance
+    def vypis(self):
+        """ Vypíše základní informace o autu"""
+        return f"Název auta: {self.znacka} \nRok výroby: {self.rok} \nModel: {self.model} \nBarva: {self.barva} \n Typ převodovky: {self.typ_prevodovky} \n Cena včetně DPH: {self.cena} Kč, \n Cena bez DPH: {self.cena_bez_dph()}"
+
+    def priplatkova_vybava(self, vybava: str, cena_vybavy: float):
+        """ Přidá příplatkovou výbavu k autu"""
+        return f"Připlatková výbava: {vybava}, cena příplatkové výbavy je: {cena_vybavy} Kč, celková cena auta s příplatkovou výbavou je: {self.cena + cena_vybavy} Kč"
+
+audi = car("Audi", "aaaa","A4", "stříbrná", "Manuální", 45000) #Instance
 skoda = car("Škoda", 2014, "Superb II", "Bílá", "Manuální", 310000)
 
-print(audi.cena)
+print(audi.rok)
 print(skoda.model)
 print(audi.vypis())
+print(f"Cena auta bez DPH je: {audi.cena_bez_dph()}")
+print(skoda.priplatkova_vybava("metalíza", 15000))
